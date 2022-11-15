5195425a6f45ceb3b0b21d9f46bfdd82975f121d
Vengadaramanan1234 <117824354+Vengadaramanan1234@users.noreply.github.com>
2022-11-15 11:07:19 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -2,10 +2,10 @@
 #include<stdlib.h>
 int main(int argc , char * argv[])
 {
-	int a = 0;
+	int a = 10;
 	int b = 0;
 	int result = 0;
-	scanf("%d %d", &a,&b);
+	//scanf("%d %d", &a,&b);
 	result = a+b;
 	printf("%d", result);
 }
