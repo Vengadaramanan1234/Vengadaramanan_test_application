e39b8eb9ce136c7b1a764c3b32695b05a7c045d9
Vengadaramanan <vengadaramananmano@gmail.com>
2022-11-15 11:02:01 +0530

    Addition program for first time

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc , char * argv[])
+{
+	int a = 0;
+	int b = 0;
+	int result = 0;
+	scanf("%d %d", &a,&b);
+	result = a+b;
+	printf("%d", result);
+}
