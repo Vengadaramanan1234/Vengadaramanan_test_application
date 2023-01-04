6b8f03c4cc3fc01310193883dff164a8c9c195d3
Vengadaramanan1234 <vengadaramananmano@gmail.com>
2023-01-04 15:41:45 +0530

    creating a new file sub.c

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc , char * argv[])
+{
+
+	int a = 2;
+	int b = 0;
+	int result = 0;
+	//scanf("%d %d", &a,&b);
+//	result = a+b;
+	printf("The result is %d", a+b);
+	return 0;
+}
